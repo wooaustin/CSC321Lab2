b4cc248b9d7f8a90c1cd9c4d2c61bff7d8b183d8
ShaheenAlemi <46461452+cpshaheen@users.noreply.github.com>
2019-10-07 11:14:40 -0700

    finished part IV

--- lab2.py
+++ lab2.py
@@ -11,6 +11,11 @@ def xor_two_str(a, b):
         xored.append(xored_value)
     return ''.join(chr(x) for x in xored)
 
+# XOR two input byte sequences, outputs as a string
+def xor_two_bytes(a, b):
+    print("a:\n",a)
+    print("b:\n",b)
+
 
 # Returns string representation of the input file
 def get_file(file):
@@ -77,32 +82,79 @@ def main():
     reverse = xor_two_str(encrypted_text, key)
     f.close()
 
-    # Part 3a
-    f = open("encrypted-cp-logo.bmp", "wb")
-    count, msg = get_bmp_file("cp-logo.bmp")
-    key_a = os.urandom(count)
-    key_a = key_a.decode('latin1')
-    preprocessed_a = preprocess_image(msg)
-    encrypted_text_a = xor_two_str(key_a, msg)
-    final_encrypt_a = replaceHeader(preprocessed_a, encrypted_text_a, f)
-    f.close()
+    #part 3
+    #first we read in both files as byte arrays
+    f_1_name = 'cp-logo.bmp'
+    f_2_name = 'mustang.bmp'
 
-    # Part 3b
-    f = open("encrypted-mustang.bmp", "wb")
-    count, msg = get_bmp_file("mustang.bmp")
-    key_b = os.urandom(count)
-    key_b = key_b.decode('latin1')
-    preprocessed_b = preprocess_image(msg)
-    # TODO For running part 4, replace key_b with key_a so both images are encrypted with the same key
-    encrypted_text_b = xor_two_str(key_a, msg)
-    final_encrypt_b = replaceHeader(preprocessed_b, encrypted_text_b, f)
-    f.close()
+    with open(f_1_name,'rb') as f:
+        data_f_1 = bytearray(f.read())
 
-    # Part 4
-    f = open("overlay_image.bmp", "wb")
-    encrypted_cp_logo = get_bmp_file("encrypted-cp-logo.bmp")
-    encrypted_mustang = get_bmp_file("encrypted-mustang.bmp")
-    encrypt_both = xor_two_str(encrypted_cp_logo, encrypted_mustang)
-    replaceHeader(preprocessed_a, encrypt_both, f)
-    f.close()
+    with open(f_2_name,'rb') as f:
+        data_f_2 = bytearray(f.read())
+
+    bmp_header = data_f_2[0:54]
+    key1 = os.urandom(len(data_f_1)-54)
+    key2 = os.urandom(len(data_f_2)-54)
+    print(type(key))
+    key_ba_1 = bytearray(key1)
+    key_ba_2 = bytearray(key2)
+    data_f_1 = data_f_1[54:]
+    data_f_2 = data_f_2[54:]
+
+    f_1_xord = bytes(a ^ b for (a,b) in zip(data_f_1,key_ba_1))
+    f_2_xord = bytes(a ^ b for (a,b) in zip(data_f_2,key_ba_2))
+
+    f_1_head_incl = bmp_header + f_1_xord
+    f_2_head_incl = bmp_header + f_2_xord
+
+    f_1_name = f_1_name[:-4] + "_xord.bmp"
+    f_2_name = f_2_name[:-4] + "_xord.bmp"
+
+    with open(f_1_name, 'wb') as f:
+        f.write(f_1_head_incl)
+
+    with open(f_2_name, 'wb') as f:
+        f.write(f_2_head_incl)
+
+    #part 4
+    #very similar to 3 - for this part we just use 1 random key
+    #                    with the goal of xoring each encrypted file
+    f_1_name = 'cp-logo.bmp'
+    f_2_name = 'mustang.bmp'
+
+    with open(f_1_name,'rb') as f:
+        data_f_1 = bytearray(f.read())
+
+    with open(f_2_name,'rb') as f:
+        data_f_2 = bytearray(f.read())
+
+    bmp_header = data_f_2[0:54]
+    key = os.urandom(len(data_f_1)-54)
+    print(type(key))
+    key_ba = bytearray(key)
+    data_f_1 = data_f_1[54:]
+    data_f_2 = data_f_2[54:]
+
+    f_1_xord = bytes(a ^ b for (a,b) in zip(data_f_1,key_ba))
+    f_2_xord = bytes(a ^ b for (a,b) in zip(data_f_2,key_ba))
+
+    f_1_head_incl = bmp_header + f_1_xord
+    f_2_head_incl = bmp_header + f_2_xord
+
+    f_1_name = f_1_name[:-4] + "_xord_IV.bmp"
+    f_2_name = f_2_name[:-4] + "_xord_IV.bmp"
+
+    with open(f_1_name, 'wb') as f:
+        f.write(f_1_head_incl)
+
+    with open(f_2_name, 'wb') as f:
+        f.write(f_2_head_incl)
+
+    combo_xor = bytes(a ^ b for (a,b) in zip(f_1_xord,f_2_xord))
+    cmb_xor_head_incl = bmp_header + combo_xor
+
+    with open("combo_xor.bmp",'wb') as f:
+        f.write(cmb_xor_head_incl)
+
 main()
